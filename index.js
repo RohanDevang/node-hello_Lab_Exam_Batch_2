f309e65eb2b9667d16e6d83878b4cb2a8cc42631
Rohan Devang <116082519+RohanDevang@users.noreply.github.com>
2025-04-15 14:28:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,12 @@ const http = require('http');
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-  console.log(`Server running on port ${PORT}`);
-});
-
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello Node!\n';
   res.end(msg);
 });
 
-server.listen(port, () => {
-  console.log(`Server running on http://localhost:${port}/`);
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on http://localhost:${PORT}/`);
 });
